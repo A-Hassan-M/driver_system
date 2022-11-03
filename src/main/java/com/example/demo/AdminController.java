eb6376493e0d69fce4a0ec3e09cc35a7b0f8cacf
ahmedmostafa8701 <ahmedmostafa8701@gmail.com>
2022-11-03 14:16:19 +0200

    Update AdminController.java
    change admin

--- src/main/java/com/example/demo/AdminController.java
+++ src/main/java/com/example/demo/AdminController.java
@@ -1,3 +1,7 @@
 public class AdminController{
-
+    String name = "ahmed mostafa mohamed ahmed";
+    String id = "20200682";
+    public void control(){
+        System.out.println("name: " + name + " id: " + id);
+    }
 }
