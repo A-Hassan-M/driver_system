3155f9ff595295fdcc13911d95d7d71929758641
Nour Okbi <73375144+nourokbi@users.noreply.github.com>
2022-11-03 19:53:41 +0200

    AdminController Modified

--- src/main/java/com/example/demo/AdminController.java
+++ src/main/java/com/example/demo/AdminController.java
@@ -2,6 +2,7 @@ public class AdminController{
     public static void main() {
         // Name: Nour Eldeen Mahmoud Okbi
         // ID: 20200608
+        // Yoo!! haha
 		System.out.println("Hello");
 	}
 }
