e991f56ee51e4192f6d02ebc4949052650d51202
ahmedddHossam <117384537+ahmedddHossam@users.noreply.github.com>
2022-11-03 19:42:54 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Starting");
 		SpringApplication.run(DemoApplication.class, args);
+		system.out.println("Ahmed Hossam Eldin fadel 20200009");
 	}
 
 }
